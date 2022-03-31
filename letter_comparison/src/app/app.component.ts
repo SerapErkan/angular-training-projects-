2d086ac07483bcc56a79419e472767e85b7aeebb
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-03-31 12:51:32 +0300

    faker

--- letter_comparison/src/app/app.component.ts
+++ letter_comparison/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import {faker} from '@faker-js/faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'letter_comparison';
+  // title = 'letter_comparison';
+  randomText:string=faker.lorem.sentence();
 }
+//https://www.npmjs.com/package/@faker-js/faker
+//random faker.lorem.sentence()
