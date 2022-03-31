6aec420660070ae3b5afd7f8d44df634a7bfe634
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-03-31 19:04:46 +0300

    currency,date,titlecase_Pipes

--- Pipes/src/app/app.component.ts
+++ Pipes/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { DatePipe } from '@angular/common';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Pipes';
+  // title = 'Pipes';
+  name = "";
+  date = "";
+  money: number = 0;
+
+  onNameChange(value: string) {
+    this.name = value;
+
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onMonetChange(value: string) {
+
+    this.money = parseFloat(value);
+
+  }
+
+
 }
