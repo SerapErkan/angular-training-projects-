e5229f788d3ea2b7095d01a8800216e573e74812
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-01 18:05:43 +0300

    pagination-active

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'directives';
+  // title = 'directives';
+
+  currentPage = 0;
+
+  images = [
+    {
+      title: " ",
+      url: 'https://images.pexels.com/photos/1391487/pexels-photo-1391487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, {
+      title: " ",
+      url: 'https://images.pexels.com/photos/35629/bing-cherries-ripe-red-fruit.jpg?cs=srgb&dl=pexels-pixabay-35629.jpg&fm=jpg'
+    }, {
+      title: " ",
+      url: 'https://images.pexels.com/photos/3338681/pexels-photo-3338681.jpeg?cs=srgb&dl=pexels-tijana-drndarski-3338681.jpg&fm=jpg'
+    }, {
+      title: " ",
+      url: 'https://images.pexels.com/photos/4110097/pexels-photo-4110097.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4110097.jpg&fm=jpg'
+    },
+
+  ]
+
+
+
+
 }
