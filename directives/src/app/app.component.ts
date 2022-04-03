e371f787d019ed599d6379785cc5707ad3683975
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-03 12:26:53 +0300

    NgIf-NgSwitch

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -23,14 +23,70 @@ export class AppComponent {
     }, {
       title: " Pagination Design",
       url: 'https://images.pexels.com/photos/4110097/pexels-photo-4110097.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4110097.jpg&fm=jpg'
-    },{
-      title:'Pagination Design',
-    
-    url:'https://images.pexels.com/photos/2683373/pexels-photo-2683373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  }
+    }, {
+      title: 'Pagination Design',
+
+      url: 'https://images.pexels.com/photos/2683373/pexels-photo-2683373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+      title: "Pagination Design",
+      url: 'https://images.pexels.com/photos/1391487/pexels-photo-1391487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, {
+      title: "Pagination Design ",
+      url: 'https://images.pexels.com/photos/35629/bing-cherries-ripe-red-fruit.jpg?cs=srgb&dl=pexels-pixabay-35629.jpg&fm=jpg'
+    }, {
+      title: "Pagination Design ",
+      url: 'https://images.pexels.com/photos/3338681/pexels-photo-3338681.jpeg?cs=srgb&dl=pexels-tijana-drndarski-3338681.jpg&fm=jpg'
+    }, {
+      title: " Pagination Design",
+      url: 'https://images.pexels.com/photos/4110097/pexels-photo-4110097.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4110097.jpg&fm=jpg'
+    }, {
+      title: 'Pagination Design',
+
+      url: 'https://images.pexels.com/photos/2683373/pexels-photo-2683373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, {
+      title: "Pagination Design",
+      url: 'https://images.pexels.com/photos/1391487/pexels-photo-1391487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, {
+      title: "Pagination Design ",
+      url: 'https://images.pexels.com/photos/35629/bing-cherries-ripe-red-fruit.jpg?cs=srgb&dl=pexels-pixabay-35629.jpg&fm=jpg'
+    }, {
+      title: "Pagination Design ",
+      url: 'https://images.pexels.com/photos/3338681/pexels-photo-3338681.jpeg?cs=srgb&dl=pexels-tijana-drndarski-3338681.jpg&fm=jpg'
+    }, {
+      title: " Pagination Design",
+      url: 'https://images.pexels.com/photos/4110097/pexels-photo-4110097.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4110097.jpg&fm=jpg'
+    }, {
+      title: 'Pagination Design',
+
+      url: 'https://images.pexels.com/photos/2683373/pexels-photo-2683373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+      title: "Pagination Design",
+      url: 'https://images.pexels.com/photos/1391487/pexels-photo-1391487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, {
+      title: "Pagination Design ",
+      url: 'https://images.pexels.com/photos/35629/bing-cherries-ripe-red-fruit.jpg?cs=srgb&dl=pexels-pixabay-35629.jpg&fm=jpg'
+    }, {
+      title: "Pagination Design ",
+      url: 'https://images.pexels.com/photos/3338681/pexels-photo-3338681.jpeg?cs=srgb&dl=pexels-tijana-drndarski-3338681.jpg&fm=jpg'
+    }, {
+      title: " Pagination Design",
+      url: 'https://images.pexels.com/photos/4110097/pexels-photo-4110097.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4110097.jpg&fm=jpg'
+    }, {
+      title: 'Pagination Design',
+
+      url: 'https://images.pexels.com/photos/2683373/pexels-photo-2683373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
   ]
 
 
+
+  pageIndex(index: number) {
+
+    return Math.abs(this.currentPage - index) < 5;
+    //sag ve soldan boşluk 
+  }
 
 
 }
